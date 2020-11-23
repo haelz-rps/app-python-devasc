13b65d5602bacab786a3b5b81c6f7f07e43623c1
haelz <raphaelbj1@gmail.com>
2020-11-23 18:47:20 -0300

    testando build docker hub

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 
 #docker tag local-image:tagname new-repo:tagname
 #docker push new-repo:tagname
-#docker push haelz/abredes-devasc:tagname
+##docker push haelz/abredes-devasc:tagname
